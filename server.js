02408e38aa32a305a8ce0f5eb2fb97b1ff04754b
Kevin <kbeens@protonmail.com>
2021-09-19 18:03:10 -0700

    PUT request to add quote added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-
+    console.log('listening in...')
 })
 
 app.get('/api/quotes/random', (req, res, next) => {
@@ -30,4 +30,21 @@ app.get('/api/quotes', (req, res, next) => {
     }
 })
 
+app.post('/api/quotes', (req, res, next) => {
+    const quote = req.query.quote
+    const person = req.query.person
+    if (quote && person) {
+        quotes.push({
+            quote:  quote,
+            person: person     
+        })
+
+        res.status(201).send({quote: {
+            quote:  quote,
+            person: person     
+        }})
+    } else {
+        res.status(400).send()
+    }
+})
 
